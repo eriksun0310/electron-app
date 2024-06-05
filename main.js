2a73c89bd0cc95e09b7494bc37ef02adb362fa65
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-06-05 10:37:07 +0800

    version 1.0.1

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, autoUpdater } = require("electron");
+
+const log = require("electron-log");
+
+// 配置日志输出
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = "info";
+log.info("App starting...");
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -24,4 +31,29 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+// 设置自动更新的来源
+autoUpdater.setFeedURL({
+  provider: "github",
+  repo: "electron-app",
+  releaseType: "release",
+});
+
+//當更新可用時
+autoUpdater.on("update-available", () => {
+  log.info("Update available.");
+});
+
+//當更新已下載完成時
+autoUpdater.on("update-downloaded", () => {
+  log.info("Update downloaded; will install now");
+  autoUpdater.quitAndInstall();
+});
+
+//在應用程式啟動時, 設置每3分鐘檢查一次, 是否要更新
+app.on("ready", () => {
+  setInterval(() => {
+    autoUpdater.checkForUpdatesAndNotify();
+  }, 3 * 60 * 1000); 
 });
