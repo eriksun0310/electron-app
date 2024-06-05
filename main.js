f1aa5b659966e9633f2828ef1d08344762917731
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2024-06-05 11:00:54 +0800

    v1.0.2

--- main.js
+++ main.js
@@ -38,6 +38,7 @@ autoUpdater.setFeedURL({
   provider: "github",
   repo: "electron-app",
   releaseType: "release",
+  url: "https://github.com/eriksun0310/electron-app",
 });
 
 //當更新可用時
@@ -52,8 +53,57 @@ autoUpdater.on("update-downloaded", () => {
 });
 
 //在應用程式啟動時, 設置每3分鐘檢查一次, 是否要更新
+// app.on("ready", () => {
+//   setInterval(() => {
+//     autoUpdater.checkForUpdatesAndNotify();
+//   }, 3 * 60 * 1000);
+// });
+
+// 定时执行自动更新检查的间隔（以毫秒为单位）
+const updateCheckInterval = 10 * 60 * 1000; // 每隔 10 分钟检查一次更新
+
+// 执行自动更新检查
+function checkForUpdates() {
+  autoUpdater.checkForUpdatesAndNotify();
+}
+
+// 当应用程序准备就绪时
 app.on("ready", () => {
-  setInterval(() => {
-    autoUpdater.checkForUpdatesAndNotify();
-  }, 3 * 60 * 1000); 
+  checkForUpdates();
+  // 每隔一定时间执行一次自动更新检查
+  setInterval(checkForUpdates, updateCheckInterval);
+
+  
+  // 当有更新可用时触发
+  autoUpdater.on("update-available", () => {
+    // 提示用户有新版本可用，并询问是否要立即更新
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "更新提示",
+        message: "有新版本可用，是否立即更新？",
+        buttons: ["是", "否"],
+      })
+      .then((response) => {
+        if (response.response === 0) {
+          // 如果用户选择立即更新，则开始下载并安装新版本
+          autoUpdater.downloadUpdate();
+        }
+      });
+  });
+
+  // 当更新下载完成时触发
+  autoUpdater.on("update-downloaded", () => {
+    // 提示用户更新已完成，并要求重新启动应用程序
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "更新完成",
+        message: "更新已完成，重启应用程序以应用更新。",
+        buttons: ["确定"],
+      })
+      .then(() => {
+        app.quit(); // 退出应用程序以应用更新
+      });
+  });
 });
